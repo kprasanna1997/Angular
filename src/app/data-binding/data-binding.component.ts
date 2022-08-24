8ea808285d2165ca5e5ca55faae95237c77156c0
unknown <kprasanna331@gmail.com>
2022-08-24 16:15:38 +0530

    This commit is regarding data-binding exercise

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+welcome:string="Welcome to Data-binding session";
+isTrue:boolean=false;
+position:string="center"
+name:string="Google";
+isCreated:string="";
+
+
+  constructor() { 
+    setTimeout(()=>{
+      this.isTrue=true;
+    },10000)
+  }
+
+  ngOnInit(): void {
+  }
+
+  serverName(event){
+   this.name=(event.target as HTMLInputElement).value
+  }
+
+  createServer(){
+  this.isCreated=`Server is created with server name ${this.name}`
+  }
+
+}
